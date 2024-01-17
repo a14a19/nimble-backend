1713e4a6f17049f7f4b970706781c406f949c73f
Sai Kumar <43190921+saikumar1767@users.noreply.github.com>
2024-01-17 22:18:36 +0530

    Sign_up commit (#2)

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -25,3 +25,102 @@ export const userSignInValidation = async (req, res, next) => {
         next();
     }
 }
+
+export const userSignUpValidation = async (req, res, next) => {
+
+    await check('fullName', 'Full Name is required!').exists().trim().run(req);
+    await check('fullName', 'Full Name should be a string!').isString().run(req);
+    await check('fullName', 'Full Name length should be at least 2 characters!').isLength({ min: 2, max: 255 }).run(req);
+
+    await check('email', 'Email is required!').exists().trim().run(req);
+    await check('email', 'Email should be a string!').isString().run(req);
+    await check('email', 'Please enter the correct format for email!').isEmail().run(req);
+    await check('email', 'Email is already registered!').custom(async (value) => {
+        const findUserEmail = await Users.find({ email: value })
+        if (findUserEmail.length > 0) {
+            return Promise.reject('Email already registered!')
+        }
+    }).run(req);
+
+    await check('password', 'Password is required!').exists().trim().run(req);
+    await check('password', 'Password should be strong!').isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minSymbols: 1 }).run(req);
+    await check('password', 'Password should be strong!').isString().run(req);
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ errors: errors.array() })
+    } else {
+        next();
+    }
+}
+
+export const userVerificationValidation = async (req, res, next) => {
+
+    await check('email', 'Email is required!').exists().trim().run(req);
+    await check('email', 'Email should be a string!').isString().run(req);
+    await check('email', 'Please enter the correct format for email!').isEmail().run(req);
+    await check('email', 'Email is already registered!').custom(async (value) => {
+        const findUserEmail = await Users.find({ email: value })
+        if (findUserEmail.length > 0) {
+            return Promise.reject('Email already registered!')
+        }
+    }).run(req);
+
+    await check('number', 'Number is required!').exists().trim().run(req);
+    await check('number', 'Please enter only numbers!').isNumeric().run(req);
+    await check('number', 'Please enter only 10 digit numbers!').isLength(10).run(req);
+    await check('number', 'Number is already registered!').custom(async (value) => {
+        const findNumber = await Users.find({ number: value })
+        if (findNumber.length > 0) {
+            return Promise.reject('Phone number already registered!')
+        }
+    }).run(req);
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ errors: errors.array() })
+    } else {
+        next();
+    }
+}
+
+export const userAuthenticationValidation = async (req, res, next) => {
+
+    await check('fullName', 'Full Name is required!').exists().trim().run(req);
+    await check('fullName', 'Full Name should be a string!').isString().run(req);
+    await check('fullName', 'Full Name length should be at least 2 characters!').isLength({ min: 2, max: 255 }).run(req);
+
+    await check('email', 'Email is required!').exists().trim().run(req);
+    await check('email', 'Email should be a string!').isString().run(req);
+    await check('email', 'Please enter the correct format for email!').isEmail().run(req);
+    await check('email', 'Email is already registered!').custom(async (value) => {
+        const findUserEmail = await Users.find({ email: value })
+        if (findUserEmail.length > 0) {
+            return Promise.reject('Email already registered!')
+        }
+    }).run(req);
+
+    await check('password', 'Password is required!').exists().trim().run(req);
+    await check('password', 'Password should be strong!').isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minSymbols: 1 }).run(req);
+    await check('password', 'Password should be strong!').isString().run(req);
+
+    await check('number', 'Number is required!').exists().trim().run(req);
+    await check('number', 'Please enter only numbers!').isNumeric().run(req);
+    await check('number', 'Please enter only 10 digit numbers!').isLength(10).run(req);
+    await check('number', 'Number is already registered!').custom(async (value) => {
+        const findNumber = await Users.find({ number: value })
+        if (findNumber.length > 0) {
+            return Promise.reject('Phone number already registered!')
+        }
+    }).run(req);
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ errors: errors.array() })
+    } else {
+        next();
+    }
+}
